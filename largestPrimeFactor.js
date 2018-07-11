848f7c3f51a787ac0dee6852f035542254376eeb
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-07-11 10:25:38 -1000

    Added solution to the Largest Prime Factor Challenge.

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,29 @@
-exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
+'use strict';
+exports.largestPrimeFactor = function (n) {
+  let primeNumber;
+  let searchSet = parseInt(Math.pow(n, (.5)));
+  let factorArray = [];
+
+  if (n > 2) {
+    primeNumber = 3;
+    factorArray = [3];
+
+  } else {
+    primeNumber = 1;
+  }
+
+  for (let i = primeNumber; i < searchSet; i += 2) {
+
+    if (n % i === 0) {
+      if (!factorArray.some(number => i % number === 0)) {
+        factorArray.push(i);
+        primeNumber = i;
+      }
+    }
+
+
+
+  }
 
   return primeNumber;
 };
